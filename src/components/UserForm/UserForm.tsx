0fcadfe38a094c0296c46b59a8bf63bf9ee0ad35
Oxozon <oxozon.o4@gmail.com>
2023-10-22 12:18:03 +0200

    init basic react app for testing

---
+++ src/components/UserForm/UserForm.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+interface UserFormProps {
+  onUserAdd: (user: { name: string, email: string }) => void;
+}
+
+const UserForm = ({ onUserAdd }: UserFormProps) => {
+  const [email, setEmail] = useState<string>('');
+  const [name, setName] = useState<string>('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    onUserAdd({ name, email });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name</label>
+        <input value={name} type='text' onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div>
+        <label>Email</label>
+        <input value={email} type='text' onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <button>Add User</button>
+    </form>
+  );
+}
+
+export default UserForm;
---
+++ src/components/UserList/UserList.tsx
@@ -0,0 +1,28 @@
+interface UserListProps {
+  users: { name: string; email: string }[];
+}
+
+const UserList = ({ users }: UserListProps) => {
+  const renderedUsers = users.map((user) => {
+    return (
+      <tr key={user.name}>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>{renderedUsers}</tbody>
+    </table>
+  );
+}
+
+export default UserList;
