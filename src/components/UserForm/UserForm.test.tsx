784d566122c6787ffd6399ac735b8813b20e3590
Oxozon <oxozon.o4@gmail.com>
2023-10-22 14:17:14 +0200

    try the argList to track callback tests

--- src/components/UserForm/UserForm.test.tsx
+++ src/components/UserForm/UserForm.test.tsx
@@ -10,3 +10,25 @@ test('it shows two inputs and a button', () => {
   expect(button).toBeInTheDocument();
 });
 
+test('it calls onUserAdd when the form is submitted', () => {
+  const argList: any = [];
+  const callback = (...args: any) => {
+    argList.push(args)
+  }
+  render(<UserForm onUserAdd={callback} />);
+
+  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+  user.click(nameInput);
+  user.keyboard('John Doe');
+
+  user.click(emailInput);
+  user.keyboard('john@doe.com');
+
+  const button = screen.getByRole('button');
+  user.click(button);
+
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({ name: 'John Doe', email: 'john@doe.com'});
+
+});
