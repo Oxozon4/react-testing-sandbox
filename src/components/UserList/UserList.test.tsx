7b644c32c512feade6734bf583735ee42d57aa82
Oxozon <oxozon.o4@gmail.com>
2023-10-25 21:48:13 +0200

    test out naive approach with roles

--- src/components/UserList/UserList.test.tsx
+++ src/components/UserList/UserList.test.tsx
@@ -7,8 +7,10 @@ test('render one row per user', () => {
   const users = [{ name: 'jane', email: 'jane@jane.com' }, { name: 'sam', email: 'sam@sam.com' }];
 
   render(<UserList users={users} />);
+  // screen.logTestingPlaygroundURL();
 
-  screen.logTestingPlaygroundURL();
+  const rows = screen.getAllByRole('row');
+  expect(rows).toHaveLength(2); // error: expected 2 to be 3
 });
 
 test('render the email and name of each user', () => {
