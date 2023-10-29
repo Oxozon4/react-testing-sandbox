345305577ba87a9abfdd2a00ba9c43d9b14394bd
Oxozon <oxozon.o4@gmail.com>
2023-10-29 13:14:35 +0100

    Add failing test case for new feature - TDD

--- src/components/UserForm/UserForm.test.tsx
+++ src/components/UserForm/UserForm.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from '../../utils/test-utils'
+import { render, screen } from '../../utils/test-utils';
 import user from '@testing-library/user-event';
-import { vi } from 'vitest'
+import { vi } from 'vitest';
 import UserForm from './UserForm';
 
 test('it shows two inputs and a button', () => {
-  render(<UserForm onUserAdd={() => {}}/>);
+  render(<UserForm onUserAdd={() => {}} />);
   const inputs = screen.getAllByRole('textbox');
   const button = screen.getByRole('button');
   expect(inputs.length).toBe(2);
@@ -29,5 +29,28 @@ test('it calls onUserAdd when the form is submitted', async () => {
   await user.click(button);
 
   expect(mock).toHaveBeenCalled();
-  expect(mock).toHaveBeenCalledWith({ name: 'John Doe', email: 'john@doe.com'});
+  expect(mock).toHaveBeenCalledWith({
+    name: 'John Doe',
+    email: 'john@doe.com',
+  });
+});
+
+test('empties the two inputs when form is submitted', async () => {
+  render(<UserForm onUserAdd={() => {}} />);
+
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
+  const button = screen.getByRole('button');
+
+  await user.click(nameInput);
+  await user.keyboard('John Doe');
+
+  await user.click(emailInput);
+  await user.keyboard('john@doe.com');
+
+  await user.click(button);
+
+  // Failing tests - TDD
+  expect(nameInput).toHaveValue('');
+  expect(emailInput).toHaveValue('');
 });
