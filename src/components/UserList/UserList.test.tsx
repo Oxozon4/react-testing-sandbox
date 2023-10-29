293ff7daed945672ad258ecd8e46ef70e3e0a395
Oxozon <oxozon.o4@gmail.com>
2023-10-29 12:32:33 +0100

    add first UserList test alternative

--- src/components/UserList/UserList.test.tsx
+++ src/components/UserList/UserList.test.tsx
@@ -1,18 +1,22 @@
-import { render, screen, within } from '../../utils/test-utils'
+import { render, within, screen } from '../../utils/test-utils';
 import user from '@testing-library/user-event';
-import { vi } from 'vitest'
+import { vi } from 'vitest';
 import UserList from './UserList';
 
-test('render one row per user', async  () => {
-  const users = [{ name: 'jane', email: 'jane@jane.com' }, { name: 'sam', email: 'sam@sam.com' }];
+test('render one row per user', async () => {
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'sam', email: 'sam@sam.com' },
+  ];
 
   render(<UserList users={users} />);
+  const rows = await within(screen.getByTestId('users')).findAllByRole('row');
+
+  // Alternative
+  // const rows = container.querySelectorAll('tbody tr');
   // screen.logTestingPlaygroundURL();
 
-  const rows = await within(screen.getByTestId('users')).findAllByRole('row');
   expect(rows).toHaveLength(2);
 });
 
-test('render the email and name of each user', () => {
-  
-});
+test('render the email and name of each user', () => {});
